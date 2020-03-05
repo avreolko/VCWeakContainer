754afb0258eac6d1c9e6435779657343e05f05fa
avreolko <vacherep@gmail.com>
2020-03-05 21:52:51 +0700

    added map

--- Sources/WeakContainer.swift
+++ Sources/WeakContainer.swift
@@ -8,9 +8,16 @@
 
 public struct Weak<T: AnyObject> {
 
-	public private(set) weak var object: T?
+    public private(set) weak var object: T?
 
-	public init(_ object: T) {
-		self.object = object
-	}
+    public init(_ object: T) {
+        self.object = object
+    }
+
+    public func map<U>(_ f: (T) -> U) -> Weak<U>? {
+        switch self.object {
+        case .some(let x): return Weak<U>(f(x))
+        case .none: return .none
+        }
+    }
 }
